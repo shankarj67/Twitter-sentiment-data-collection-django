53171f1a2d1ea180e98dcda2361f4cb989ac2202
Shankar Jha <shankarj67@gmail.com>
2020-05-09 16:53:54 +0530

    Create the project and add necessary settings:  - Add an quiz app  - Add django-debug-toolbar
    Fixes #3 and #6

---
+++ conf/urls.py
@@ -0,0 +1,19 @@
+
+from django.contrib import admin
+from django.conf import settings
+from django.urls import include, path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+
+        # For django versions before 2.0:
+        # url(r'^__debug__/', include(debug_toolbar.urls)),
+
+    ] + urlpatterns
